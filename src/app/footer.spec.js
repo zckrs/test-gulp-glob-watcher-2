701cb190727bfd44b7db685d5477e0ac61596d9b
Mehdy Dara <mdara@eleven-labs.com>
2016-04-15 11:34:40 +0200

    Use glob-watcher-2 Gulp branch

---
+++ src/app/footer.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {Footer} from './footer';
+
+describe('Footer', () => {
+  it('should be a footer', () => {
+    expect(shallow(<Footer/>).is('footer')).toBe(true);
+  });
+});
