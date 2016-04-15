cc08823ff445b5de0574c22fb10a93a73bea2279
Mehdy Dara <mdara@eleven-labs.com>
2016-04-15 11:33:47 +0200

    Scaffold with `yo fountain-webapp`

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'es6-promise';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Main} from './app/main';
+
+import './index.scss';
+
+ReactDOM.render(
+  <Main/>,
+  document.getElementById('root')
+);
